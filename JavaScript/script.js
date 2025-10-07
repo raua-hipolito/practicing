713478ff352266e8bc3e48ebc44483bf878373a6
Rauã Hipólito <rauavasconcelos22@gmail.com>
2025-10-07 12:48:38 -0300

    Exemplo if/else

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,9 @@
-let nota1 = 222;
-let nota2 = 222;
+let nota1 = "22";
+let nota2 = "222";
 
 if (nota1 > nota2) {
   console.log("Nota 1 vencedora!");
-} else if (nota1 == nota2) {
+} else if (nota1 === nota2) {
   console.log("Tivemos um empate!");
 } else {
   console.log("Nota 2 vencedora!!");
