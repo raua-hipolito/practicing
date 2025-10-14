390eee40f5bef5413dbd448c951f282bda7d844b
Rauã Hipólito <rauavasconcelos22@gmail.com>
2025-10-13 22:07:04 -0300

    feat: adicionar verificação de nota com condicional

--- JavaScript/if-else.js
+++ JavaScript/if-else.js
@@ -1,9 +1,20 @@
-let grade = 6;
+// let grade = 6;
 
-if (grade >= 8) {
-  console.log("Parabéns, aprovado!");
-} else if (grade === 8 || grade >= 6) {
-  console.log("Lista de espera ");
+// if (grade >= 8) {
+//   console.log("Parabéns, aprovado!");
+// } else if (grade === 8 || grade >= 6) {
+//   console.log("Lista de espera ");
+// } else {
+//   console.log("reprovado");
+// }
+
+let notaAluno = 6.5;
+let notaMedia = 7;
+
+if (notaAluno == 10) {
+  console.log("Aprovado com Distinção");
+} else if (notaAluno >= notaMedia) {
+  console.log("Aprovado");
 } else {
-  console.log("reprovado");
+  console.log("Reprovado");
 }
