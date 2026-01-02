86704caf8490ff34f4f8ebce8d9446faa37005f8
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-02 17:40:06 -0300

    feat: criando pequeno sistema de notas com For e if

--- JavaScript/laços de repetição/lacos.js
+++ JavaScript/laços de repetição/lacos.js
@@ -1,17 +1,41 @@
-let frutas = [
-  "Maçã",
-  "Banana",
-  "Laranja",
-  "Uva",
-  "Manga",
-  "Abacaxi",
-  "Melancia",
-  "Morango",
-  "Pera",
-  "Kiwi",
+// let frutas = [
+//   "Maçã",
+//   "Banana",
+//   "Laranja",
+//   "Uva",
+//   "Manga",
+//   "Abacaxi",
+//   "Melancia",
+//   "Morango",
+//   "Pera",
+//   "Kiwi",
+// ];
+
+// for (let frutas1 = 0; frutas1 < frutas.length; frutas1++) {
+//   const element = frutas[frutas1];
+//   console.log(element);
+// }
+
+const alunos = [
+  { nome: "Ana", nota: 8.5 },
+  { nome: "Bruno", nota: 7.0 },
+  { nome: "Carlos", nota: 9.2 },
+  { nome: "Daniela", nota: 6.8 },
+  { nome: "Eduardo", nota: 8.0 },
+  { nome: "Fernanda", nota: 9.5 },
+  { nome: "Gabriel", nota: 7.8 },
+  { nome: "Helena", nota: 8.9 },
+  { nome: "Igor", nota: 6.5 },
+  { nome: "Juliana", nota: 9.0 },
 ];
 
-for (let frutas1 = 0; frutas1 < frutas.length; frutas1++) {
-  const element = frutas[frutas1];
-  console.log(element);
+for (let i = 0; i < alunos.length; i++) {
+  const element = alunos[i];
+  console.log(`Aluno: ${alunos[i].nome} nota: ${alunos[i].nota}`);
+
+  if (alunos[i].nota >= 7) {
+    console.log("Aprovado");
+  } else {
+    console.log("Reprovado");
+  }
 }
