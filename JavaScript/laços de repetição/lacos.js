e36c65b15bd9ab3aa3b2d6bc2bc713ac67af4b09
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-02 17:01:06 -0300

    feat: criando um pequeno laço

---
+++ JavaScript/laços de repetição/lacos.js
@@ -0,0 +1,17 @@
+let frutas = [
+  "Maçã",
+  "Banana",
+  "Laranja",
+  "Uva",
+  "Manga",
+  "Abacaxi",
+  "Melancia",
+  "Morango",
+  "Pera",
+  "Kiwi",
+];
+
+for (let frutas1 = 0; frutas1 < frutas.length; frutas1++) {
+  const element = frutas[frutas1];
+  console.log(element);
+}
