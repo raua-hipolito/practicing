3480e99b42da36015200ac811d31e13e6ec04db2
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-05 14:20:39 -0300

    feat: numeros divisiveis por 3

--- JavaScript/laços de repetição/lacos.js
+++ JavaScript/laços de repetição/lacos.js
@@ -57,3 +57,16 @@ for (let index = 1; index <= 10; index++) {
     console.log(index);
   }
 }
+
+// // ------------------------------
+
+let count = 0;
+
+for (let i = 2; i <= 150; i++) {
+  if (i % 3 === 0) {
+    count++;
+    if (count === 50) {
+      console.log("Mensagem secreta!");
+    }
+  }
+}
