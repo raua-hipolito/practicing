418e08e6324dc9a5b5f35be6629811498114aeb7
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-05 15:22:02 -0300

    feat: resolvendo exercicio: verificando se é vogal ou consoante

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,15 @@
-let nota1 = "22";
-let nota2 = "222";
+const input = require("readline-sync");
 
-if (nota1 > nota2) {
-  console.log("Nota 1 vencedora!");
-} else if (nota1 === nota2) {
-  console.log("Tivemos um empate!");
-} else {
-  console.log("Nota 2 vencedora!!");
+const vogais = ["a", "e", "i", "o", "u"];
+
+let inputTest = input.question("Digite uma letra: ").toLowerCase();
+
+function verificaVogais() {
+  if (vogais.includes(inputTest)) {
+    console.log("É uma vogal");
+  } else {
+    console.log("É uma consoante");
+  }
 }
+
+verificaVogais();
