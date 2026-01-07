7878e9d66639e9b777444cdcc0c610802885d120
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-07 17:44:58 -0300

    feat: verificando com if se a senha é correta

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -24,15 +24,25 @@ const input = require("readline-sync");
 //     A mensagem "Reprovado", se a média for menor do que sete;
 //     A mensagem "Aprovado com Distinção", se a média for igual a dez.
 
-let nota1 = Number(input.question("Digite sua primeira nota: "));
-let nota2 = Number(input.question("Digite sua segunda nota: "));
+// let nota1 = Number(input.question("Digite sua primeira nota: "));
+// let nota2 = Number(input.question("Digite sua segunda nota: "));
 
-let media = (nota1 + nota2) / 2;
+// let media = (nota1 + nota2) / 2;
 
-if (media === 10) {
-  console.log("Aprovado com Distinção");
-} else if (media >= 7) {
-  console.log("Aprovado");
+// if (media === 10) {
+//   console.log("Aprovado com Distinção");
+// } else if (media >= 7) {
+//   console.log("Aprovado");
+// } else {
+//   console.log("Reprovado");
+// }
+
+// ------------------------------
+
+let senha = Number(input.question("Digite sua senha: "));
+
+if (senha == 1234) {
+  console.log("Acesso permitido");
 } else {
-  console.log("Reprovado");
+  console.log("Acesso negado");
 }
