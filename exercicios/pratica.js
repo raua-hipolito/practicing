a967289321ec140ace826461d12e7d1767260c76
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-09 19:58:39 -0300

    melhorando ambiente de trabalho

---
+++ exercicios/pratica.js
@@ -0,0 +1,11 @@
+let nome = "Sheila";
+let cargo = "gerência";
+let senhaDeAcesso = "Senha!123#";
+
+if (nome === "Sheila" && cargo === "gerência") {
+  if (senhaDeAcesso == "Senha!123#") {
+    console.log("Acesso liberado para gerente");
+  } else {
+    console.log("Acesso negado!");
+  }
+}
