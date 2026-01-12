e5fcbf2b8920a80176030a3c5271f2f1f4992a70
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-12 11:57:00 -0300

    adicionando e substituindo valor em um array pelo indice e pelo metodo push

--- JavaScript/Arrays/array.js
+++ JavaScript/Arrays/array.js
@@ -8,3 +8,46 @@ const pizzas = [
 ];
 
 console.log(pizzas[3]);
+
+// Adicionando um valor a um array
+
+const listaDeCompras = ["arroz", "feijão", "ovo"];
+
+listaDeCompras.push("farofa");
+
+console.log(listaDeCompras);
+
+// ------------------------------// ------------------------------// ------------------------------
+
+const titulosCopaDoMundo = [1958, 1962, 1970, 1994];
+
+titulosCopaDoMundo[4] = 2002;
+
+console.log(titulosCopaDoMundo);
+
+// Substituindo um item em um array
+
+const estadosNorte = [
+  "Acre",
+  "Amapá",
+  "Amazonas",
+  "Paraná",
+  "Rondônia",
+  "Roraima",
+  "Tocantins",
+];
+
+estadosNorte[3] = "Pará";
+console.log(estadosNorte);
+
+// ------------------------------// ------------------------------// ------------------------------
+
+const ingredientes = ["arroz", "batata", "cenoura", "sal", "pimenta do reino"];
+
+ingredientes[2] = "suco";
+
+console.log(ingredientes);
+
+// De olho na dica 👀 : Para facilitar a manipulação de listas, sem precisar escrevê-las do zero quando quiser substituir um valor, utilize o índice do item que você quer substituir.
+
+// Recuperando o tamanho de um array
