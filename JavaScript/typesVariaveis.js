e3259d3a10d47efa55d28d3d38c3154637830a52
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-12 16:34:36 -0300

    feat: utilizando template literals

--- JavaScript/typesVariaveis.js
+++ JavaScript/typesVariaveis.js
@@ -1,8 +1,19 @@
-const firstname = "Rauã";
-let lastname = "Hipólito";
-let age = 21;
-age = 21;
+// const firstname = "Rauã";
+// let lastname = "Hipólito";
+// let age = 21;
+// age = 21;
+// console.log(firstname + " " + lastname + " Sua idade:" + age);
 
-lastname = "Silva";
+// console.log(`Bem vindo ${firstname + " " + lastname}`);
 
-console.log(firstname + " " + lastname + " Sua idade:" + age);
+const product = "iphone15";
+const price = 2500;
+const discont = 10;
+
+const messege = `
+Produto: ${product}
+Preço: ${price}
+Desconto: ${discont}%
+Preço com desconto: ${price - price * (discont / 100)}`;
+
+console.log(messege);
