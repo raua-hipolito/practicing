688115797367340c8f7d5bae72c8959e6c45747f
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-12 16:54:14 -0300

    feat: pedindo nome e idade de usuario e utilizando templete literals para exibir mensagem

--- JavaScript/typesVariaveis.js
+++ JavaScript/typesVariaveis.js
@@ -1,3 +1,5 @@
+const input = require("readline-sync");
+
 // const firstname = "Rauã";
 // let lastname = "Hipólito";
 // let age = 21;
@@ -5,15 +7,23 @@
 // console.log(firstname + " " + lastname + " Sua idade:" + age);
 
 // console.log(`Bem vindo ${firstname + " " + lastname}`);
+// // ------------------------------// // ------------------------------// // ------------------------------// // ------------------------------
 
-const product = "iphone15";
-const price = 2500;
-const discont = 10;
+// const product = "iphone15";
+// const price = 2500;
+// const discont = 10;
 
-const messege = `
-Produto: ${product}
-Preço: ${price}
-Desconto: ${discont}%
-Preço com desconto: ${price - price * (discont / 100)}`;
+// const messege = `
+// Produto: ${product}
+// Preço: ${price}
+// Desconto: ${discont}%
+// Preço com desconto: ${price - price * (discont / 100)}`;
 
-console.log(messege);
+// console.log(messege);
+
+// // ------------------------------// // ------------------------------// // ------------------------------
+
+const nomeUsuario = input.question("Insira seu nome: ");
+const idadeUsuario = Number(input.question("Insira sua idade: "));
+
+console.log(`Olá, meu nome é ${nomeUsuario} e tenho ${idadeUsuario} anos.`);
