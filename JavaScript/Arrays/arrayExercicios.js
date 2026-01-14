2804eba262546071d62f21650442c2c1af955a69
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-14 14:51:08 -0300

    fear: questão 3: listando posição e cor com for

--- JavaScript/Arrays/arrayExercicios.js
+++ JavaScript/Arrays/arrayExercicios.js
@@ -1,10 +1,23 @@
 const frutas = ["banana", "maça", "uva", "acerola", "manga"];
 
-console.log(frutas[0]);
-console.log(frutas[frutas.length - 1]);
+// console.log(frutas[0]);
+// console.log(frutas[frutas.length - 1]);
 
 // ======================================================================================//===================================================================
 
 const nomePessoas = ["Rauã", "Lucas", "Marcos", "Kaue"];
 
-console.log(`O array possui ${nomePessoas.length} elementos.`);
+// console.log(`O array possui ${nomePessoas.length} elementos.`);
+
+// ======================================================================================//===================================================================
+
+const cores = ["azul", "preto", "rosa", "cinza"];
+
+// console.log(`Cor na posição 0: ${cores[0]}`);
+// console.log(`Cor na posição 1: ${cores[1]}`);
+// console.log(`Cor na posição 2: ${cores[2]}`);
+// console.log(`Cor na posição 3: ${cores[3]}`);
+
+for (let i = 0; i < cores.length; i++) {
+  console.log(`Cor na posição ${i}: ${cores[i]}`);
+}
