cb157ec6629976b01d3de8c3674db6f81199baf7
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-14 14:24:10 -0300

    feat: exercicio array

--- JavaScript/Arrays/array.js
+++ JavaScript/Arrays/array.js
@@ -87,3 +87,20 @@ shoppingList.pop();
 shoppingList.shift();
 
 console.log(shoppingList.length);
+
+// Exercício 1
+// Altere o valor da variável menuServices para que ela passe a ter o valor “Serviços”. Isso deve ser feito através da variável menu.
+
+const menu = ["Home", "Serviços", "Portfólio", "Links"];
+const menuServices = menu[1];
+
+console.log(menuServices);
+
+// Exercício 2
+// Adicione o valor “Contato” no final do array menu.
+
+const menu1 = ["Home", "Serviços", "Portfólio", "Links"];
+
+menu1.push("Contato");
+
+console.log(menu1);
