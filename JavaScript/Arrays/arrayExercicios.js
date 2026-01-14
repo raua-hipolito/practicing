e45ad51bac16340c8cb8cfe7f681ffbf0089042f
Rauã Hipólito <rauavasconcelos22@gmail.com>
2026-01-14 14:43:00 -0300

    feat: resolvendo questão 2: verificando quantos elementos o array possui + template literals

--- JavaScript/Arrays/arrayExercicios.js
+++ JavaScript/Arrays/arrayExercicios.js
@@ -2,3 +2,9 @@ const frutas = ["banana", "maça", "uva", "acerola", "manga"];
 
 console.log(frutas[0]);
 console.log(frutas[frutas.length - 1]);
+
+// ======================================================================================//===================================================================
+
+const nomePessoas = ["Rauã", "Lucas", "Marcos", "Kaue"];
+
+console.log(`O array possui ${nomePessoas.length} elementos.`);
